e965dba0c718556c9d35ee680026985c73b91366
stevgomezt <97990647+stevgomezt@users.noreply.github.com>
2024-04-16 17:30:39 -0500

    avances

---
+++ src/components/Formulario.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="md:w-1/2">
+        <h2 class="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+        <p class="text-lg mt-5 text-center mb-10">
+            Registra un Paciente y
+            <span class="text-indigo-600 font-bold"> Administralos </span>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1 class="font-black text-5xl text-center">
+        Seguimiento Pacientes
+        <span class="text-indigo-600">Veterinaria</span>
+    </h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
