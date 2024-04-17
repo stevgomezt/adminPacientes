2026ac67832d0ade364a3dc248ba3754009b81ed
stevgomezt <97990647+stevgomezt@users.noreply.github.com>
2024-04-17 17:44:51 -0500

    avances

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,43 @@
     <div class="container mx-auto mt-20"></div>
     <Header />
     <div class="mt-12 md:flex">
-        <Formulario />
+        <Formulario
+            v-model:mascota="paciente.mascota"
+            v-model:propietario="paciente.propietario"
+            v-model:email="paciente.email"
+            v-model:alta="paciente.alta"
+            v-model:sintomas="paciente.sintomas"
+            @guardar-paciente="guardarPaciente"
+        />
+        <div class="md:w-1/2 md:h-screen overflow-y-scroll">
+            <h3 class="font-black text-3xl text-center">
+                Administra tus Pacientes
+            </h3>
+            <div v-if="pacientes.length > 0"></div>
+            <p v-else class="mt-10 text-2xl text-center">No hay Pacientes</p>
+        </div>
     </div>
 </template>
 
 <script setup>
+import { ref, reactive } from "vue";
 import Header from "./components/Header.vue";
 import Formulario from "./components/Formulario.vue";
+
+const pacientes = ref([]);
+
+const paciente = reactive({
+    mascota: "",
+    propietario: "",
+    email: "",
+    alta: "",
+    sintomas: "",
+});
+
+const guardarPaciente = () => {
+    console.log("agregando...");
+    pacientes.value.push(paciente);
+};
 </script>
 
 <style lang="scss" scoped></style>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -5,11 +5,149 @@
             Registra un Paciente y
             <span class="text-indigo-600 font-bold"> Administralos </span>
         </p>
+
+        <Alerta v-if="alerta.mensaje" :alerta="alerta" />
+
+        <form
+            action=""
+            class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+            v-on:submit.prevent="validar"
+        >
+            <div class="mb-5">
+                <label
+                    for="mascota"
+                    class="block text-gray-700 uppercase font-bold"
+                    >Nombre Mascota</label
+                >
+                <input
+                    type="text"
+                    id="mascota"
+                    placeholder="Nombre de la mascota"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    :value="mascota"
+                    @input="$emit('update:mascota', $event.target.value)"
+                />
+            </div>
+            <div class="mb-5">
+                <label
+                    for="propietario"
+                    class="block text-gray-700 uppercase font-bold"
+                    >Nombre del Propietario</label
+                >
+                <input
+                    type="text"
+                    id="propietario"
+                    placeholder="Nombre del propietario"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    :value="propietario"
+                    @input="$emit('update:propietario', $event.target.value)"
+                />
+            </div>
+            <div class="mb-5">
+                <label
+                    for="email"
+                    class="block text-gray-700 uppercase font-bold"
+                    >Email</label
+                >
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Email del propietario"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    :value="email"
+                    @input="$emit('update:email', $event.target.value)"
+                />
+            </div>
+            <div class="mb-5">
+                <label
+                    for="alta"
+                    class="block text-gray-700 uppercase font-bold"
+                    >Alta</label
+                >
+                <input
+                    type="date"
+                    id="alta"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    :value="alta"
+                    @input="$emit('update:alta', $event.target.value)"
+                />
+            </div>
+            <div class="mb-5">
+                <label
+                    for="sintomas"
+                    class="block text-gray-700 uppercase font-bold"
+                    >Sintomas</label
+                >
+                <textarea
+                    type="text"
+                    id="sintomas"
+                    placeholder="Sintomas de la mascota"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
+                    :value="sintomas"
+                    @input="$emit('update:sintomas', $event.target.value)"
+                />
+            </div>
+            <input
+                type="submit"
+                name=""
+                id=""
+                class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+                value="Registrar Paciente"
+            />
+        </form>
     </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { reactive } from "vue";
+import Alerta from "./Alerta.vue";
+
+const alerta = reactive({
+    tipo: "",
+    mensaje: "",
+});
+
+const emit = defineEmits([
+    "update:mascota",
+    "update:propietario",
+    "update:email",
+    "update:alta",
+    "update:sintomas",
+    "guardar-paciente",
+]);
+
+const props = defineProps({
+    mascota: {
+        type: String,
+        required: true,
+    },
+    propietario: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    alta: {
+        type: String,
+        required: true,
+    },
+    sintomas: {
+        type: String,
+        required: true,
+    },
+});
+
+const validar = () => {
+    if (Object.values(props).includes("")) {
+        alerta.mensaje = "Todos los campos son obligatorios...";
+        alerta.tipo = "error";
+        return;
+    }
+
+    emit("guardar-paciente");
+};
 </script>
 
 <style lang="scss" scoped></style>
